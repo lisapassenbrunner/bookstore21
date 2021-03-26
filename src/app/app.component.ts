16fece6735a2e7a638f87f85b020feb1069d03bd
lisapassenbrunner <s1810456024@students.fh-hagenberg.at>
2021-03-26 11:05:15 +0100

    2 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-@Component({selector: 'bs-root',
-template: '<bs-book-list></bs-book-list>',
-styles: []
+import { Component } from "@angular/core";
+@Component({
+  selector: "bs-root",
+  template: "<bs-book-list></bs-book-list>",
+  styles: []
 })
-export class AppComponent { }
+export class AppComponent {}
---
+++ src/app/book-list-item/book-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { Book, Author, Image } from "../shared/book";
+
+@Component({
+  selector: "a.bs-book-list-item",
+  templateUrl: "./book-list-item.component.html",
+  styleUrls: ["./book-list-item.component.css"]
+})
+export class BookListItemComponent implements OnInit {
+  @Input() book: Book;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
