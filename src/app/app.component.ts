18cd4c777fca24df7029d91085b635191ee79d6f
lisapassenbrunner <s1810456024@students.fh-hagenberg.at>
2021-03-26 10:47:43 +0100

    5 files modified, 6 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
-import { Component, VERSION } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+import { Component } from '@angular/core';
+@Component({selector: 'bs-root',
+template: '<bs-book-list></bs-book-list>',
+styles: []
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-
+import { BookListComponent } from './book-list/book-list.component';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+declarations: [
+AppComponent,
+BookListComponent
+],
+imports: [
+BrowserModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
