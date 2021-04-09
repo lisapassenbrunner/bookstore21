dbd0e46d64b6cf1814c16d583dc3e828f47e413d
lisapassenbrunner <s1810456024@students.fh-hagenberg.at>
2021-04-09 09:26:33 +0200

    2 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,29 +2,9 @@ import { Component } from "@angular/core";
 import { Book } from "./shared/book";
 @Component({
   selector: "bs-root",
-  template: `
-    <bs-book-list
-      *ngIf="listOn"
-      (showDetailsEvent)="showDetails($event)"
-    ></bs-book-list>
-    <bs-book-details
-      *ngIf="detailsOn"
-      [book]="book"
-      (showListEvent)="showList()"
-    ></bs-book-details>
-  `
+  templateUrl: './app.component.html'
+  
 })
 export class AppComponent {
-  listOn = true;
-  detailsOn = false;
-  book: Book;
-  showList() {
-    this.listOn = true;
-    this.detailsOn = false;
-  }
-  showDetails(book: Book) {
-    this.book = book;
-    this.listOn = false;
-    this.detailsOn = true;
-  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,17 @@ import { BookListItemComponent } from "./book-list-item/book-list-item.component
 import { BookDetailsComponent } from "./book-details/book-details.component";
 
 import { BookStoreService } from "./shared/book-store.service";
+import { HomeComponent } from './home/home.component';
+import{AppRoutingModule} from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [
     AppComponent,
     BookListComponent,
     BookListItemComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
+    HomeComponent
   ],
   bootstrap: [AppComponent],
   providers: [BookStoreService]
