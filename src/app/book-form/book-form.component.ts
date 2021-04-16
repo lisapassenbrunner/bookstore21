c53aae8973e7968c256b354ff402cd328f6b76a1
lisapassenbrunner <s1810456024@students.fh-hagenberg.at>
2021-04-16 09:33:16 +0200

    2 files modified, 1 added

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -17,6 +17,7 @@ import { BookStoreService } from "../shared/book-store.service";
 })
 export class BookFormComponent implements OnInit {
   bookForm: FormGroup;
+  //zwar leer, aber existiert schon
   book = BookFactory.empty();
   isUpdatingBook = false;
   images: FormArray;
@@ -28,5 +29,34 @@ export class BookFormComponent implements OnInit {
     private router: Router
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const isbn = this.route.snapshot.params["isbn"];
+    //asynchroner Block
+    if (isbn) {
+      this.isUpdatingBook = true;
+      this.bs.getSingle(isbn).subscribe(book => {
+        this.book = book;
+        this.initBook();
+      });
+    }
+    this.initBook();
+  }
+
+  initBook() {
+    this.bookForm = this.fb.group({
+      id: this.book.id,
+      title: [this.book.title, Validators.required],
+      subtitle: this.book.subtitle,
+      isbn: [
+        this.book.isbn,
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(13)
+        ]
+      ],
+      description: this.book.description,
+      published: this.book.published
+    });
+  }
 }
