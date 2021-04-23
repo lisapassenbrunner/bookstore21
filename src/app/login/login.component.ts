d3f8b5c2bcc6f15073af83ffd8ade3897baab063
lisapassenbrunner <s1810456024@students.fh-hagenberg.at>
2021-04-23 11:43:22 +0200

    3 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,10 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { AuthenticationService } from "../shared/authentication.service";
 
+interface Response {
+  access_token: string;
+}
+
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
@@ -17,10 +21,10 @@ export class LoginComponent implements OnInit {
     private authService: AuthenticationService
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.loginForm = this.fb.group({
       username: ["", [Validators.required, Validators.email]],
-      password: ["", [Validators.required]]
+      password: ["", Validators.required]
     });
   }
 
@@ -29,6 +33,7 @@ export class LoginComponent implements OnInit {
     if (val.username && val.password) {
       this.authService.login(val.username, val.password).subscribe(res => {
         console.log(res);
+        this.authService.setLocalStorage((res as Response).access_token);
       });
     }
   }
--- src/app/shared/authentication.service.ts
+++ src/app/shared/authentication.service.ts
@@ -1,28 +1,31 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
-interface Token{
+interface Token {
   exp: number;
   user: {
     id: string;
-  }
+  };
 }
 
 @Injectable({
-    providedIn: 'root'
-  })
-
+  providedIn: "root"
+})
 export class AuthenticationService {
-  private api = "https://bookstore21.s1810456024.student.kwmhgb.at/api";
+  private api: string =
+    "https://bookstore21.s1810456024.student.kwmhgb.at/api/auth";
 
-  constructor(private http: HttpClient){
-
-  }
+  constructor(private http: HttpClient) {}
 
   login(email: string, password: string) {
     return this.http.post(`${this.api}/login`, {
       email: email,
       password: password
     });
+  }
+
+  public setLocalStorage(token: string) {
+    console.log("storing token");
+    console.log(jwt_decode(token));
   }
 }
