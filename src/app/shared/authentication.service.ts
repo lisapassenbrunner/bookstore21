f2b07a74d28bde0f69ff564546f200e6a2bcb080
lisapassenbrunner <s1810456024@students.fh-hagenberg.at>
2021-04-24 09:05:28 +0200

    1 file modified

--- src/app/shared/authentication.service.ts
+++ src/app/shared/authentication.service.ts
@@ -1,43 +1,73 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import jwt_decode from "jwt-decode";
 
 interface Token {
   exp: number;
   user: {
     id: string;
-  };
+  }
 }
 
+
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class AuthenticationService {
-  private api: string =
-    "https://bookstore21.s1810456024.student.kwmhgb.at/api/auth";
 
-  constructor(private http: HttpClient) {}
+  private api:string = 'https://bookstore21.s1810456024.student.kwmhgb.at/api/auth';
 
-  login(email: string, password: string) {
+  constructor(private http: HttpClient) { }
+
+  login (email: string, password: string) {
     return this.http.post(`${this.api}/login`, {
       email: email,
       password: password
     });
   }
 
-  public setLocalStorage(token: string) {
-    console.log("storing token");
-    console.log(jwt_decode(token));
+  public setLocalStorage (token: string) {
+    console.log("storing stoken");
+   
     const decodedToken = jwt_decode(token) as Token;
+    console.log(decodedToken.user.id);
     sessionStorage.setItem("token", token);
     sessionStorage.setItem("userId", decodedToken.user.id);
+
   }
 
-  public getCurrentUserId(){
-    return Number.parseInt({sessionStorage.getItem("userId"));
+  public getCurrentUserId() {
+    return Number.parseInt(sessionStorage.getItem("userId"));
   }
 
-  logout(){
-    this.http.post(`${this.api}/logout, `)
+  logout() {
+    this.http.post(`${this.api}/logout`, {});
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("userId");
+    console.log("logged out");
   }
+
+  public isLoggedIn() {
+
+    if (sessionStorage.getItem("token")) {
+      let token: string = sessionStorage.getItem("token");
+      const decodedToken = jwt_decode(token) as Token;
+      let expirationDate: Date = new Date(0);
+      expirationDate.setUTCSeconds(decodedToken.exp);
+      if (expirationDate < new Date()) {
+        console.log("token expired");
+        sessionStorage.removeItem("token");
+        return false;
+      }
+      return true;
+    } else {
+      return false; 
+    }
+
+  }
+
+  public isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
 }
